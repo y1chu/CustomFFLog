2e75b8d0c534cb455359e476d6821b6fa171b0ca
Ingrid Chu <78563674+IngridChu@users.noreply.github.com>
2022-02-24 17:36:33 -0800

    Added Maven

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
+import javax.swing.text.Document;
+import java.net.*;
+import java.io.*;
+import java.sql.Connection;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        // <div id="graph-and-table-container">
+        String page = "http://www.something.com/";
+        //Connecting to the web page
+        Connection conn = Jsoup.connect(page);
+        //executing the get request
+        Document doc = conn.get();
+        //Retrieving the contents (body) of the web page
+        String result = doc.body().text();
+        System.out.println(result);
+
+
+
+
+    }
+
+
 }
+
+
+
